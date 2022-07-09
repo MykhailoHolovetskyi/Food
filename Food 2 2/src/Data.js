8d06efc4dcf5a4c27704fca3ea79f3b737e0c357
MykhailoHolovetskyi <mykhaylo194@gmail.com>
2022-07-09 20:28:54 +0300

    addProject

---
+++ Food 2 2/src/Data.js
@@ -0,0 +1,57 @@
+let shopItemsData = [{
+    id: 'yrtpou',
+    name: 'bigMac',
+    price: 45,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/medium_4063.jpeg'
+},
+{
+    id: 'qclpo',
+    name: 'awf',
+    price: 10,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/Sdwch_Cheeseburger.jpeg'
+},
+{
+    id: 'sldfkgls',
+    name: 'ghfhj',
+    price: 20,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/Sdwch_Hamburger.jpeg'
+},
+{
+    id: 'zmxlaasc',
+    name: 'qqqqqqq',
+    price: 100,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/thumb.Sdwch_BigMac.jpeg'
+}];
+
+let shopItemsDataKfc = [{
+    id: 'qqqaa',
+    name: 'KFC',
+    price: 45,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/3737.jpeg'
+},
+{
+    id: 'wwwsss',
+    name: 'KFC1',
+    price: 10,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/4061.jpeg'
+},
+{
+    id: 'eeeddddd',
+    name: 'KFC2',
+    price: 20,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/4065.jpeg'
+},
+{
+    id: 'rrffff',
+    name: 'KFC3',
+    price: 100,
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    img: '/images/4064.jpeg'
+}];
